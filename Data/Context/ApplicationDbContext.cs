082b40299a7f8a051b8da90a49d087675173a38d
Iojecht <markcayco@hotmail.com>
2015-07-16 10:02:56 +0800

    Project Structure

---
+++ Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base(nameOrConnectionString: "ApplicationDbContext")
+        {
+            
+        }
+
+        //public DbSet<Table> TableName { get; set; }
+    }
+}
